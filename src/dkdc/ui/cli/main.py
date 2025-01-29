5b34b7f8203529fc86300276dfc49cbdb1331ec0
Cody <cody@dkdc.dev>
2025-01-29 12:02:19 -0500

    remove gui, slim down

--- src/dkdc/ui/cli/main.py
+++ src/dkdc/ui/cli/main.py
@@ -2,16 +2,11 @@
 import typer
 
 from dkdc.ui.console import print
-from dkdc.ui.cli.todo import todo_app
 from dkdc.ui.cli.common import default_kwargs
 
 # typer config
 ## main app
 app = typer.Typer(help="dkdc", **default_kwargs)
-
-## add sub-apps
-app.add_typer(todo_app, name="todo")
-app.add_typer(todo_app, name="t", hidden=True)
 
 
 # commands
@@ -53,35 +48,6 @@ def open(
         list_things()
     else:
         open_it(thing)
-
-
-## servers
-@app.command()
-@app.command("g", hidden=True)
-def gui(
-    port: int = typer.Option(1913, help="port", show_default=True),
-    prod: bool = typer.Option(False, help="prod?", show_default=True),
-):
-    """
-    gui
-    """
-    from shiny import run_app as run_gui_app
-    from dkdc.ui.gui import app  # noqa
-
-    if prod:
-        run_gui_app(
-            app=app,
-            host="0.0.0.0",
-            port=port,
-        )
-    else:
-        run_gui_app(
-            app="dkdc.ui.gui:app",  # goofy! but needed to reload
-            host="0.0.0.0",
-            port=port,
-            reload=True,
-            launch_browser=True,
-        )
 
 
 # if __name__ == "__main__":
--- src/dkdc/ui/gui/__init__.py
+++
@@ -1,3 +0,0 @@
-from dkdc.ui.gui.main import app
-
-__all__ = ["app"]
